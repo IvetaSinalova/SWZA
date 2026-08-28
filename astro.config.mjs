587aa200e1f107cf566c4e9846d99958821a1721
IvetaSinalova <iveta.sinalova78@gmail.com>
2026-08-28 22:41:07 +0200

    presmerovanie na spravne css clasy added

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,2 +1,6 @@
 import { defineConfig } from 'astro/config';
-export default defineConfig({ site: 'https://www.startupweekendzilina.sk', output: 'static' });
+export default defineConfig({
+  site: 'https://www.startupweekendzilina.sk',
+  base: process.env.ASTRO_BASE_PATH || '/',
+  output: 'static',
+});
